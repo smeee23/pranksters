8c378ccfbc468a29ffe5b5481b6f7d330c7bf797
smeee <smeee23@gmail.com>
2023-01-23 15:19:37 -0500

    Updates

---
+++ precache-manifest.cf6535fb691f2de7710e1d0931a90653.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "535bf2493a30f30b51cf2dacf108bda5",
+    "url": "./index.html"
+  },
+  {
+    "revision": "ce57106bcc27c8860fb9",
+    "url": "./static/css/main.3fd16072.chunk.css"
+  },
+  {
+    "revision": "51e98a990d6436b13c32",
+    "url": "./static/js/2.19a870f3.chunk.js"
+  },
+  {
+    "revision": "ce57106bcc27c8860fb9",
+    "url": "./static/js/main.0449ff7b.chunk.js"
+  },
+  {
+    "revision": "fec49678728315511c82",
+    "url": "./static/js/runtime-main.8d6e6c54.js"
+  },
+  {
+    "revision": "cead68153cc27046895cca70c9a65bb3",
+    "url": "./static/media/dubsportz_black.cead6815.png"
+  },
+  {
+    "revision": "1e5f745f756b8e9dfe821a72a1a56cac",
+    "url": "./static/media/dubsportz_blue.1e5f745f.png"
+  },
+  {
+    "revision": "651dc708b292b6b12bb27dd2890bd657",
+    "url": "./static/media/grass.651dc708.jpeg"
+  }
+]);
